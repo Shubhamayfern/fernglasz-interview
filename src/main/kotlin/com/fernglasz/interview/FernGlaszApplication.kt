040eaaeefd93d6d140908a84962501c0cb710d2a
Shubhamay <shubhamaydas1@gmail.com>
2024-02-21 22:03:46 +0100

    initial commit

---
+++ src/main/kotlin/com/fernglasz/interview/FernGlaszApplication.kt
@@ -0,0 +1,11 @@
+package com.fernglasz.interview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FernGlaszApplication
+
+fun main(args: Array<String>) {
+	runApplication<FernGlaszApplication>(*args)
+}
---
+++ src/test/kotlin/com/fernglasz/interview/FernGlaszApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fernglasz.interview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FernGlaszApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
